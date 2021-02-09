5b90ea00552f27fe4f11a652ca9f4d141c96528d
ansarali41 <ansar.ict14@gmail.com>
2021-02-10 01:07:49 +0600

    file upload using multer

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const fileController = require('../controllers/file.controller');
 
 /* GET home page. */
 router.get('/', function (req, res) {
     // res.render('index', { title: 'Express' });
     res.send('Hello World');
 });
+
+router.post('/upload', fileController.upload.single('myFile'), fileController.fileUpload);
 
 // export default router;
 module.exports = router;
